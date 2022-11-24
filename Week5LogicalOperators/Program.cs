e7e5e6a9aeab5964c37587aef4a24efe5933881d
Mikk Soomre <mikk@soomre.com>
2022-11-24 15:39:38 +0200

    add Week5 Code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿// AND - &&
+
+// true && true && true --> true
+// true && false --> false
+// false && true --> false
+// false && false --> false
+
+// OR - II (pipes)
+// true II true --> true
+// true II false --> true
+// false II true --> true
+// false II false --> false
